0e4a5fec8e26e3c10dc825bde2932165847dd74d
victor.monzo.mora <victor.monzo.mora@gmail.com>
2021-03-09 11:06:39 +0100

    first commit

---
+++ app/Models/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    // Crear relación Uno a muchos con la tabla Users
+    public function users() {
+        return $this->belongsTo('App\Models\User', 'user_id');
+    }
+
+    public function states() {
+        return $this->belongsTo('App\Models\State', 'state');
+    }
+
+    // Crear relación Uno a muchos con la tabla Products
+    public function products() {
+        return $this->belongsTo('App\Models\Product', 'product_id');
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    // Crear relación Uno a muchos con la tabla OrderLines
+    public function orders() {
+        return $this->hasMany('App\Models\Product');
+    }
+
+    // Crear relación Uno a muchos con la tabla Providers
+    public function providers() {
+        return $this->belongsTo('App\Models\Provider', 'provider_id');
+    }
+}
